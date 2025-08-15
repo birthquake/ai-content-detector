9bce2aada76998f26b5afe14fff10a369cf4944d
birthquake <rashiedtyre@gmail.com>
2025-08-15 13:43:09 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Force cache busting on deployments
+  generateBuildId: async () => {
+    return `build-${Date.now()}`
+  },
+  // Disable service worker caching for development
+  swcMinify: true,
 }
 
 module.exports = nextConfig
