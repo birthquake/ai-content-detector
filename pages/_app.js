1b86db8b037d96d79b2a90b7ad89b469c6c614f2
birthquake <rashiedtyre@gmail.com>
2025-08-15 14:40:11 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
-import '../styles/globals.css'
+// pages/_app.js
+import '../styles/globals.css';
+import { AuthProvider } from '../lib/AuthContext';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
